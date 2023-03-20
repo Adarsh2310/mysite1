504d878be6d13ac4a86231f571704488c69fb7f9
Adarsh2310 <adarshpan96@gmail.com>
2023-03-20 17:00:53 +0530

    django practise for begineers

--- formapp/views.py
+++ formapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import forms
+
 
 # Create your views here.
+def userRegistrationview(request):
+    form = forms.UserRegistration
+    return render(request, 'templatesapp/userRegistration.html', {'form': form})
